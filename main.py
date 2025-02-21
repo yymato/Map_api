06d9709664a909467ccd9e053683ba0557839b6c
yymato <070913a@gmail.com>
2025-02-21 16:25:52 +0300

    1-я задача

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
+import requests
+
+from ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    g_map: QLabel
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.map_zoom = 5
+        self.map_ll = [37.977751, 55.757718]
+        self.map_l = 'map'
+        self.map_key = ''
+
+        self.refresh_map()
+
+    def refresh_map(self):
+        map_params = {
+            "ll": ','.join(map(str, self.map_ll)),
+            "l": self.map_l,
+            'z': self.map_zoom
+        }
+
+        response = requests.get('https://static-maps.yandex.ru/1.x/', params=map_params)
+        with open('tmp.png', mode='wb') as tmp:
+            tmp.write(response.content)
+
+        pixmap = QPixmap()
+        pixmap.load('tmp.png')
+        self.g_map.setPixmap(pixmap)
+
+
+app = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+sys.exit(app.exec())
